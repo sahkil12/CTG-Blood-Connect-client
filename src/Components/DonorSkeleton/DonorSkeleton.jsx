8efbcba619be91b7b44483f5c7f301052c1c738d
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-18 20:34:57 +0600

    impliment skeleton loading effect in donor card to look more good

---
+++ src/Components/DonorSkeleton/DonorSkeleton.jsx
@@ -0,0 +1,11 @@
+const DonorSkeleton = () => {
+     return (
+          <div className="animate-pulse bg-white rounded-xl shadow p-5">
+               <div className="h-40 bg-gray-200 rounded mb-4"></div>
+               <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+               <div className="h-4 bg-gray-200 rounded mt-2"></div>
+          </div>
+     );
+};
+
+export default DonorSkeleton;
--- src/Pages/Home/DonorList/DonorList.jsx
+++ src/Pages/Home/DonorList/DonorList.jsx
@@ -1,11 +1,7 @@
-import Loader from "../../../Components/Loader/Loader";
+import DonorSkeleton from "../../../Components/DonorSkeleton/DonorSkeleton";
 import DonorCard from "./DonorCard";
 
 const DonorList = ({ donors, loading }) => {
-
-     if (loading) {
-          return <Loader></Loader>
-     }
 
      return (
           <section className="bg-base-100">
@@ -14,9 +10,15 @@ const DonorList = ({ donors, loading }) => {
                          Available Donors
                     </h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                         {donors.map((donor) => (
-                              <DonorCard key={donor._id} donor={donor} />
-                         ))}
+                         {loading &&
+                              Array.from({ length: 9 }).map((_, i) => (
+                                   <DonorSkeleton key={i} />
+                              ))}
+
+                         {!loading &&
+                              donors.map((donor) => (
+                                   <DonorCard key={donor._id} donor={donor} />
+                              ))}
                     </div>
                </div>
           </section>
