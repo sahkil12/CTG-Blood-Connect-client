9c6b8efcabd99fba6ad9743753efb8b9a3d59229
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-21 23:00:26 +0600

    update in profile csrd component

--- src/Pages/Profile/ProfileCard.jsx
+++ src/Pages/Profile/ProfileCard.jsx
@@ -1,36 +1,35 @@
 const ProfileCard = ({ donor }) => {
-  return (
-    <div className="bg-white border rounded-xl shadow-md p-8 grid grid-cols-1 md:grid-cols-3 gap-8">
-      
-      {/* Left */}
-      <div className="text-center">
-        <img
-          src={donor.profileImage}
-          alt={donor.name}
-          className="w-40 h-40 mx-auto rounded-full object-cover"
-        />
-        <h3 className="text-xl font-semibold mt-4">{donor.name}</h3>
-        <p className="text-sm text-gray-500">{donor.bloodGroup}</p>
-        <span className="inline-block mt-2 px-4 py-1 text-sm rounded-full bg-green-100 text-green-700">
-          {donor.status}
-        </span>
-      </div>
+     return (
+          <div className="bg-gray-100 border border-neutral-200 rounded-xl shadow-lg p-8 lg:p-10 grid grid-cols-1 md:grid-cols-3 gap-8 ">
+               {/* Left */}
+               <div className="text-center">
+                    <img
+                         src={donor.profileImage}
+                         alt={donor.name}
+                         className="w-40 h-40 lg:w-44 lg:h-44 mx-auto rounded-full object-cover"
+                    />
+                    <h3 className="text-xl font-semibold mt-5">{donor.name}</h3>
+                    <p className="text-sm md:text-lg pt-2 font-bold text-red-500">{donor.bloodGroup}</p>
+                    <span className="inline-block mt-2 px-4 py-1 text-sm rounded-full bg-green-100 text-green-800">
+                         {donor.status}
+                    </span>
+                  
+               </div>
+               {/* Right */}
+               <div className="md:col-span-2 space-y-3">
+                    <p><strong>Email:</strong> {donor.email}</p>
+                    <p><strong>Phone:</strong> {donor.phone}</p>
+                    <p><strong>Gender:</strong> {donor.gender}</p>
+                    <p><strong>Area:</strong> {donor.area}</p>
+                    <p><strong>Address:</strong> {donor.address}</p>
+                    <p>
+                         <strong>Last Donate:</strong>{" "}
+                         {donor.lastDonateDate || "Not yet"}
+                    </p>
+               </div>
 
-      {/* Right */}
-      <div className="md:col-span-2 space-y-3">
-        <p><strong>Email:</strong> {donor.email}</p>
-        <p><strong>Phone:</strong> {donor.phone}</p>
-        <p><strong>Gender:</strong> {donor.gender}</p>
-        <p><strong>Area:</strong> {donor.area}</p>
-        <p><strong>Address:</strong> {donor.address}</p>
-        <p>
-          <strong>Last Donate:</strong>{" "}
-          {donor.lastDonateDate || "Not yet"}
-        </p>
-      </div>
-
-    </div>
-  );
+          </div>
+     );
 };
 
 export default ProfileCard;
