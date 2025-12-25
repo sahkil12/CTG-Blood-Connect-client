ea57ad00255ab67fda579d4510215469113fbc09
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-12-25 11:52:17 +0600

    add firebase importan data in env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,14 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDLfNeKGSr_6Ooosrd8XFpK1oRqaxQxS6Q",
-  authDomain: "ctg-blood-connect.firebaseapp.com",
-  projectId: "ctg-blood-connect",
-  storageBucket: "ctg-blood-connect.firebasestorage.app",
-  messagingSenderId: "319633771072",
-  appId: "1:319633771072:web:d9bca6a80d3e110152eda5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
